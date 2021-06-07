d4f81b58cbf5f948e1e5cbde8fd0a39f6efec8b2
Natanev92 <nvillasenor1@icloud.com>
2021-06-06 18:18:21 -0700

    recognize

---
+++ Fundamentals/recognize_python.py/file.py
@@ -0,0 +1,87 @@
+num1 = 42 #storing an integer
+num2 = 2.3 # storing a float
+boolean = True # boolean value
+string = 'Hello World' # a string
+pizza_toppings = ['Pepperoni', 'Sausage', 'Jalepenos', 'Cheese', 'Olives'] # a list of strings
+person = {'name': 'John', 'location': 'Salt Lake', 'age': 37, 'is_balding': False} # dictionary
+fruit = ('blueberry', 'strawberry', 'banana')#tuple
+print(type(fruit))#output: tuple
+print(pizza_toppings[1])# output: sausage
+pizza_toppings.append('Mushrooms')# adds mushrooms to the end of pizza-toppings
+print(person['name'])#outout: john
+person['name'] = 'George'# reassining the value
+person['eye_color'] = 'blue'
+print(fruit[2])#output banana
+
+if num1 > 45:
+    print("It's greater")
+else:
+    print("It's lower")
+
+if len(string) < 5:
+    print("It's a short word!")
+elif len(string) > 15:
+    print("It's a long word!")
+else:
+    print("Just right!")
+
+for x in range(5):
+    print(x)
+for x in range(2,5):
+    print(x)
+for x in range(2,10,3):
+    print(x)
+x = 0
+while(x < 5):
+    print(x)
+    x += 1
+
+pizza_toppings.pop() #removing value from the end of an array
+pizza_toppings.pop(1) # removing value from index 1 in array
+
+print(person)#logging output of dictionary
+person.pop('eye_color')
+print(person)
+
+for topping in pizza_toppings:
+    if topping == 'Pepperoni':
+        continue
+    print('After 1st if statement')
+    if topping == 'Olives':
+        break
+
+def print_hello_ten_times():
+    for num in range(10):
+        print('Hello')
+
+print_hello_ten_times()
+
+def print_hello_x_times(x):
+    for num in range(x):
+        print('Hello')
+
+print_hello_x_times(4)
+
+def print_hello_x_or_ten_times(x = 10):
+    for num in range(x):
+        print('Hello')
+
+print_hello_x_or_ten_times()
+print_hello_x_or_ten_times(4)
+
+
+"""
+Bonus section
+"""
+
+# print(num3)
+# num3 = 72
+# fruit[0] = 'cranberry'
+# print(person['favorite_team'])
+# print(pizza_toppings[7])
+#   print(boolean)
+# fruit.append('raspberry')
+# fruit.pop(1)
+
+
+
