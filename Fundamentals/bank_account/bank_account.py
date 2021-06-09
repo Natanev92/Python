21c18b27cfc5870afb50ca6bb6a3348faab2a93c
Natanev92 <nvillasenor1@icloud.com>
2021-06-08 18:34:55 -0700

    bank account

---
+++ Fundamentals/bank_account/bank_account.py
@@ -0,0 +1,21 @@
+class BankAccount:
+    def __init__(self, int_rate, balance): 
+        self.int_rate = int_rate
+        self.balance = balance
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+    def withdraw(self, amount):
+        self.balance -= amount
+        return self
+    def display_account_info(self):
+        self.int_rate().balance()
+        return self
+    def yield_interest(self):
+        self.balance += (self.balance *self.int_rate)
+ba1 = BankAccount(.01, 7600)
+ba2 = BankAccount(.01, 4200)
+ba1.deposit(120).deposit(400).deposit(280).withdraw(700).yield_interest()
+print(ba1.balance)
+ba2.deposit(120).deposit(400).withdraw(500).withdraw(250).yield_interest()
+print(ba2.balance)
